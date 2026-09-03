38cd01cdc14c289341a96bd2e2f416091998cfe6
Luiza Rangel <luiza.g.rangel@aluno.senai.br>
2026-09-03 09:02:06 -0300

    COMMIT: começando a justar as cores + design

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,8 +1,12 @@
+import styles from "./Square.module.css";
+
 export default function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button
+      className={styles.square} //fa\ o react pegar a classe square
+      onClick={onSquareClick}
+    >
       {value}
     </button>
   );
 }
-
